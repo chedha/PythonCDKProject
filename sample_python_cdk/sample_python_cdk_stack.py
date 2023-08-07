e781b6688374c4988870694588770fb6ab15c041
Chris Hall <chedha@gmail.com>
2023-08-07 18:30:03 -0400

    Deploy lambda with hitcounter that logs api calls to dynamodb

--- sample_python_cdk/sample_python_cdk_stack.py
+++ sample_python_cdk/sample_python_cdk_stack.py
@@ -6,8 +6,12 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
     aws_s3 as s3
 )
+
+from hitcounter import HitCounter
 
 
 class SamplePythonCdkStack(Stack):
@@ -15,7 +19,24 @@ class SamplePythonCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        bucket = s3.Bucket(self, "mySpecialBucket-62477")
+        # bucket = s3.Bucket(self, "mySpecialBucket-62477")
+
+        my_lambda = _lambda.Function(
+            self, "HelloHandler",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            code=_lambda.Code.from_asset("lambda"),
+            handler="hello.handler"
+        )
+
+        hello_with_counter = HitCounter(
+            self, "HelloHitCounter",
+            downstream=my_lambda,
+        )
+
+        apigw.LambdaRestApi(
+            self, "Endpoint",
+            handler=hello_with_counter.handler,
+        )
 
         # queue = sqs.Queue(
         #     self, "SamplePythonCdkQueue",
