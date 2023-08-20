6ca61cb41196dc2f11678976cd0916f144058ad7
Chris Hall <chedha@gmail.com>
2023-08-20 12:11:45 -0400

    lambda response update

--- lambda/hello.py
+++ lambda/hello.py
@@ -7,5 +7,5 @@ def handler(event, context):
         "headers": {
             "Content-Type": "text/plain"
         },
-        "body": "Hello CDK!! You've successfully invoked the lambda {}\n".format(event["path"])
+        "body": "You've successfully invoked the lambda {}\n".format(event["path"])
     }
